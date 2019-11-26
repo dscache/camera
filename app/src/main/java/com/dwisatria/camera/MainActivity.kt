d80d6aced9ae1de2a4987e7a811fdc6e970aaaa5
Dwi Satria <satrs16@gmail.com>
2019-11-26 21:00:08 +0700

    26nov9pm

--- app/src/main/java/com/dwisatria/camera/MainActivity.kt
+++ app/src/main/java/com/dwisatria/camera/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.dwisatria.camera
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.graphics.Bitmap
@@ -22,15 +23,56 @@ class MainActivity : AppCompatActivity() {
 
             startActivityForResult(i,123)
         }
+        img_pick_btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_DENIED){
+                    val permission = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE);
+                    requestPermissions(permission,PERMISSION_CODE);
+                }else{
+                    pickImageFromGallery();
+                }
+            }else{
+                pickImageFromGallery();
+            }
+        }
     }
 
+    private fun pickImageFromGallery(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
+    }
 
+    companion object{
+        private val IMAGE_PICK_CODE=1000;
+        private val PERMISSION_CODE=1001;
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    pickImageFromGallery()
+                }else{
+                    Toast.makeText(this,"Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode==123){
             var bmp=data?.extras?.get("data")as?Bitmap
             imageView.setImageBitmap(bmp)
+        }
+
+        if (requestCode== Activity.RESULT_OK && requestCode== IMAGE_PICK_CODE){
+            imageView.setImageURI(data?.data)
         }
     }
 
